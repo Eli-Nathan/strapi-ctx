8b77f37111df4df576347d395e59db8b8e777062
Eli <eli.nathan@fanduel.com>
2023-06-12 11:27:00 +0100

    Add middleware function directly

--- src/api/author/routes/author.js
+++ src/api/author/routes/author.js
@@ -3,6 +3,27 @@
 /**
  * author router
  */
+const populateList = ["books"];
+const enrichCtx = (ctx) => {
+  console.log(`
+    ==========
+    ${ctx}
+    ==========
+  `);
+  if (!ctx.query) {
+    ctx.query = {};
+  }
+  if (!ctx.query.pagination) {
+    ctx.query.pagination = {};
+  }
+  ctx.query.pagination = {
+    ...ctx.query.pagination,
+    pageSize: ctx.query.pagination?.pageSize || 100,
+  };
+  const currentPopulateList = ctx.query.populate || [];
+  ctx.query.populate = [...currentPopulateList, ...populateList];
+  return ctx;
+};
 
 module.exports = {
   routes: [
@@ -10,7 +31,15 @@ module.exports = {
       method: "GET",
       path: "/authors",
       handler: "author.find",
-      middlewares: ["api::author.enrich-author"],
+      config: {
+        middlewares: [
+          async (context, next) => {
+            const newCtx = enrichCtx(context);
+            context = newCtx;
+            await next();
+          },
+        ],
+      },
     },
   ],
 };
--- src/api/book/routes/book.js
+++ src/api/book/routes/book.js
@@ -3,6 +3,15 @@
 /**
  * book router
  */
+const populateList = ["author"];
+const enrichCtx = (ctx) => {
+  if (!ctx.query) {
+    ctx.query = {};
+  }
+  const currentPopulateList = ctx.query.populate || [];
+  ctx.query.populate = [...currentPopulateList, ...populateList];
+  return ctx;
+};
 
 module.exports = {
   routes: [
@@ -10,7 +19,15 @@ module.exports = {
       method: "GET",
       path: "/books",
       handler: "book.find",
-      middlewares: ["api::book.enrich-book"],
+      config: {
+        middlewares: [
+          async (context, next) => {
+            const newCtx = enrichCtx(context);
+            context = newCtx;
+            await next();
+          },
+        ],
+      },
     },
   ],
 };
